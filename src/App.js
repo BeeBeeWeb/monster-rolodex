a3eeb9d11224c226b72373353c53357ad75f7441
bhnbhushan <babar.bhushan@gmail.com>
2021-01-14 22:30:22 +0530

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component {
 
@@ -20,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name="Bhushan"/>
         {
           this.state.monsters.map((monster) => (
             <h1 key={monster.id}>{monster.name}</h1>
